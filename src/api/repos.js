4bd3922df378d0f179ba2ff480b5603f055084d6
Ania Buwaj <100519699+ania221B@users.noreply.github.com>
2025-11-24 19:33:30 +0100

    added a safeguard

--- src/api/repos.js
+++ src/api/repos.js
@@ -4,5 +4,5 @@ export async function fetchRepos (username, sortBy, order) {
   const { data } = await api.get(
     `/search/repositories?q=user:${username}&sort=${sortBy}&order=${order}&per_page=10`
   )
-  return data.items
+  return Array.isArray(data.items) ? data.items : []
 }
